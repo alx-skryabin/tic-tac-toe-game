87c1e9a685320d719665e6366a185c0bd423678f
A.Skriabin <A.Skriabin@asntl.ru>
2023-04-26 16:21:52 +0300

    modify logic AI. Add score and saving. Restyle

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,18 @@ const victories = [
   [0, 4, 8],
   [2, 4, 6]
 ]
+const score = {
+  'o': 0,
+  'x': 0,
+  'ai': 0
+}
 
 const notify = (res: string) => {
   notification.open({
     message: res,
     description: 'Congratulations!',
     duration: 2,
-    icon: <i className="fa-regular fa-face-grin-tongue-wink"/>,
+    icon: <i className="fa-regular fa-face-grin-tongue-wink"/>
   })
 }
 
@@ -38,10 +43,22 @@ const getRandomCell = (arr: string[]): number | boolean => {
   return isFreeCell
 }
 
+const getStorageAI = (): boolean => {
+  const val = window.localStorage.getItem('AI')
+  return val ? JSON.parse(val) : false
+}
+
+const getScore = () => {
+  const val = window.localStorage.getItem('score')
+  return val ? JSON.parse(val) : score
+}
+
 function App() {
   const [end, setEnd] = useState<boolean>(false)
   const [player, setPlayer] = useState<'o' | 'x'>('o')
-  const [ai, setAi] = useState<boolean>(false)
+  const [ai, setAi] = useState<boolean>(getStorageAI())
+  const [score, setScore] = useState(getScore())
+
   const [initial, setInitial] = useState<string[]>(fields)
 
   const clickCell = (e: any) => {
@@ -57,77 +74,144 @@ function App() {
       setPlayer('o')
     }
 
-    checkWinner().then(res => {
-      if (!res) return
+    if (checkTie()) {
       setEnd(true)
-      notify(res.toString())
-      return true
-    }).then((isFinish) => {
-      if (isFinish) return
+      return notify(`Friendship won!`)
+    }
+
+    const win = checkWinner()
+
+    if (win) {
+      setEnd(true)
+      updateScore(win)
+      notify(`Winner ${win.toString().toUpperCase()}`)
+    } else {
       if (ai) aiCell()
-    })
+    }
+  }
+
+  const updateScore = (winner: string | boolean) => {
+    if (!winner) return
+
+    const upScore = {...score}
+
+    if (winner === 'o') upScore['o'] = ++upScore['o']
+    if (winner === 'x') {
+      const key = ai ? 'ai' : 'x'
+      upScore[key] = ++upScore[key]
+    }
+
+    window.localStorage.setItem('score', JSON.stringify(upScore))
+    setScore(upScore)
+  }
+
+  // Функция для получения хода AI
+  const getAiMove = () => {
+    // Попытка найти выигрышный ход
+    for (let i = 0; i < initial.length; i++) {
+      if (initial[i] !== 'o' && initial[i] !== 'x') {
+        const temp = initial[i]
+        initial[i] = 'x'
+        if (checkWinner() === 'x') {
+          initial[i] = temp
+          return i
+        }
+        initial[i] = temp
+      }
+    }
+
+    // Попытка заблокировать выигрышный ход человека
+    for (let i = 0; i < initial.length; i++) {
+      if (initial[i] !== 'o' && initial[i] !== 'x') {
+        const temp = initial[i]
+        initial[i] = 'o'
+        if (checkWinner() === 'o') {
+          initial[i] = temp
+          return i
+        }
+        initial[i] = temp
+      }
+    }
+
+    // Возвращаем случайный ход
+    return getRandomCell(initial)
   }
 
   const aiCell = () => {
     setTimeout(() => {
-      const AI = getRandomCell(initial)
+      const AI = getAiMove()
       if (Number.isInteger(AI)) {
         initial[Number(AI)] = 'x'
         setPlayer('o')
 
-        checkWinner().then(res => {
-          if (!res) return
+        const win = checkWinner()
+
+        if (win) {
           setEnd(true)
-          notify(res.toString())
-        })
+          updateScore(win)
+          notify(`Winner ${win.toString().toUpperCase()}`)
+        }
       }
     }, 100)
   }
 
+  // Функция рестарта
   const handleReset = () => {
     setPlayer('o')
     setEnd(false)
     setInitial(initial.map((_: any, i: number) => (++i).toString()))
   }
 
-  const checkWinner = async () => {
+  const checkWinner = (): string | boolean => {
     let isWinner: string | boolean = false
     victories.map((arr: number[]) => {
       if (initial[arr[0]] === 'o' && initial[arr[1]] === 'o' && initial[arr[2]] === 'o') {
-        isWinner = 'Winner O'
+        isWinner = 'o'
       }
       if (initial[arr[0]] === 'x' && initial[arr[1]] === 'x' && initial[arr[2]] === 'x') {
-        isWinner = 'Winner X'
+        isWinner = 'x'
       }
     })
     return isWinner
   }
 
+  // Функция для проверки, есть ли ничья
+  function checkTie(): boolean {
+    for (let i = 0; i < initial.length; i++) {
+      if (initial[i] !== 'o' && initial[i] !== 'x') {
+        return false
+      }
+    }
+
+    return true
+  }
+
   const activateAi = (res: boolean) => {
+    window.localStorage.setItem('AI', JSON.stringify(res))
     handleReset()
     setAi(res)
   }
 
   return (
     <div className="as__app">
-      <div className="as__player">Player <span>{player}</span> walks</div>
-
       <div className="as__control">
+        <div className="as__btn-switch">
+          <div>2 players <i className="fa-solid fa-people-pulling"/></div>
+          <Switch onChange={activateAi} checked={getStorageAI()}/>
+          <div>AI robot <i className="fa-solid fa-gun"/></div>
+        </div>
+
         <Button
           className="as__btn-reset"
           type="primary"
-          icon={<i className="fa-solid fa-wand-magic-sparkles"/>}
+          icon={<i className="fa-solid fa-right-left"/>}
           onClick={handleReset}
         >
-          Reset
+          RESTART
         </Button>
-
-        <div className="as__btn-switch">
-          <div>2 players</div>
-          <Switch onChange={activateAi}/>
-          <div>AI robot</div>
-        </div>
       </div>
+
+      <div className="as__player">Player <span>{player}</span> walks</div>
 
       <div className={`as__box ${!end || 'as__box_disabled'}`} onClick={clickCell}>
         {
@@ -136,6 +220,15 @@ function App() {
             return <div key={i} className={`as__box_item ${selectedClass}`} data-item={i}>{item}</div>
           })
         }
+      </div>
+
+      <div className="as__score">
+        <p><i className="fa-solid fa-star-half-stroke"/> Score:</p>
+        <div className="as__score-list">
+          <span>O: {score['o']}</span>
+          <span>X: {score['x']}</span>
+          <span>AI: {score['ai']}</span>
+        </div>
       </div>
 
       <div className="as__feedback">
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client'
 import App from './App'
 import './index.scss'
 
+console.log('%c ❤ The strongest will win. Good luck!', 'color: lime')
 const container = document.getElementById('root')
 const root = createRoot(container)
 
